c341524fab5a1ea38bab04ce743261bae34b19cc
Sai <saisgr1@gmail.com>
2021-01-02 17:06:46 +0100

    changes to testPassword

--- testPassword.py
+++ testPassword.py
@@ -1,9 +1,10 @@
 import unittest
 import Password
 
-class testPassword(unittest.TestCase):
-    def test1(self):
-        True
-
+class TestPassword(unittest.TestCase):
+    def test_hash_password_hash_check(self):
+        hashed_pwd = Password.hash_password(self.password)
+        self.assertTrue(Password.hash_check(self.password, hashed_pwd), (True))
+        
 if __name__ == '__main__':
     unittest.main()
